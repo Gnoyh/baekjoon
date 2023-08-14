80de79d7fe6479bc945e96c92e57db15a70e62f6
Gnoyh <4ksenhs@gmail.com>
2023-08-14 21:29:52 +0900

    commit 5622

---
+++ baekjoon_5622.py
@@ -0,0 +1,20 @@
+# https://www.acmicpc.net/problem/5622
+
+check_list = []
+
+for i in range(3, 11):
+    if i == 8 or i == 10:
+        for j in range(4):
+            check_list.append(i)
+    else:
+        for j in range(3):
+            check_list.append(i)
+
+input_str = input()
+
+result_int = 0
+
+for i in input_str:
+    result_int += check_list[ord(i) - 65]
+
+print(result_int)
