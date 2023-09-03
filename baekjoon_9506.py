4cb30c7d1fc70169d4794fefcc40eabf08616ea7
Gnoyh <4ksenhs@gmail.com>
2023-09-03 19:14:56 +0900

    commit 9506

---
+++ baekjoon_9506.py
@@ -0,0 +1,22 @@
+# https://www.acmicpc.net/problem/9506
+
+n = int(input())
+
+while n != -1:
+    check_sum = 0
+    check_str = "{} =".format(n)
+
+    for i in range(1, n):
+        if n % i == 0:
+            check_sum += i
+            check_str += " {} +".format(i)
+
+            if check_sum > n:
+                break
+
+    if check_sum == n:
+        print(check_str[: len(check_str) - 2])
+    else:
+        print("%d is NOT perfect." %n)
+
+    n = int(input())
