dddddb4f55b3ef685ba342f31f4f0c6f695acec9
Gnoyh <4ksenhs@gmail.com>
2023-09-04 12:31:55 +0900

    commit 2581

--- baekjoon_2581.py
+++ baekjoon_2581.py
@@ -13,18 +13,17 @@ for i in range(M, N + 1):
         result_sum += 2
         result_min = 2
     elif i != 1 and i % 2 == 1:
-        for j in range(3, i // 2 + 1):
+        for j in range(3, i // 3 + 1, 2):
             if i % j == 0:
                 check = 1
 
                 break
 
         if check == 0:
+            result_sum += i
+
             if result_min == 0:
-                result_sum += i
                 result_min = i
-            else:
-                result_sum += i
 
 if result_sum != 0:
     print(result_sum)
