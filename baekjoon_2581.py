3527ca92c35c4c6057007b473c822a9d47cb1108
Gnoyh <4ksenhs@gmail.com>
2023-09-04 13:02:38 +0900

    commit 2581

--- baekjoon_2581.py
+++ baekjoon_2581.py
@@ -6,24 +6,25 @@ N = int(input())
 result_sum = 0
 result_min = 0
 
+check_list = [1 for i in range(N + 1)]
+
+check_list[0] = 0
+check_list[1] = 0
+
+for i in range(4, N + 1, 2):
+    check_list[i] = 0
+
+for i in range(3, N // 3 + 1, 2):
+    if check_list[i] == 1:
+        for j in range(i * 3, N + 1, i):
+            check_list[j] = 0
+
 for i in range(M, N + 1):
-    check = 0
+    if check_list[i] == 1:
+        result_sum += i
 
-    if i == 2:
-        result_sum += 2
-        result_min = 2
-    elif i != 1 and i % 2 == 1:
-        for j in range(3, i // 3 + 1, 2):
-            if i % j == 0:
-                check = 1
-
-                break
-
-        if check == 0:
-            result_sum += i
-
-            if result_min == 0:
-                result_min = i
+        if result_min == 0:
+            result_min = i
 
 if result_sum != 0:
     print(result_sum)
