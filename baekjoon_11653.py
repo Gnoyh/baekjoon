d0b92108514af95956d9529343ac74a8b2b921b8
Gnoyh <4ksenhs@gmail.com>
2023-09-05 12:59:47 +0900

    commit 11653

--- baekjoon_11653.py
+++ baekjoon_11653.py
@@ -2,8 +2,18 @@
 
 N = int(input())
 
+check = N
+
 for i in range(2, N + 1):
     while N % i == 0:
         print(i)
 
         N /= i
+
+    if N == 1:
+        break
+
+    if N == check and i > check // 3:
+        print(check)
+
+        break
