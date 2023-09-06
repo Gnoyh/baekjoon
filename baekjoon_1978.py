c85a6ba7f3102865c9d349b795f68a1f46940a49
Gnoyh <4ksenhs@gmail.com>
2023-09-06 17:41:54 +0900

    commit 1978

--- baekjoon_1978.py
+++ baekjoon_1978.py
@@ -4,21 +4,23 @@ N = int(input())
 
 input_list = list(map(int, input().split()))
 
+check_list = [1 for i in range(max(input_list) + 1)]
+
+check_list[0] = 0
+check_list[1] = 0
+
 result = 0
 
+for i in range(4, max(input_list) + 1, 2):
+    check_list[i] = 0
+
+for i in range(3, int(max(input_list) ** 0.5) + 1, 2):
+    if check_list[i] == 1:
+        for j in range(i * 3, max(input_list) + 1, i):
+            check_list[j] = 0
+
 for i in input_list:
-    check = 0
-
-    if i == 2:
+    if check_list[i] == 1:
         result += 1
-    elif i != 1 and i % 2 == 1:
-        for j in range(3, i // 2 + 1):
-            if i % j == 0:
-                check = 1
-
-                break
-
-        if check == 0:
-            result += 1
 
 print(result)
