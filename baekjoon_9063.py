e1d366a1b1df73ca3a7ce5195933d090b7477c00
Gnoyh <4ksenhs@gmail.com>
2023-09-09 21:39:34 +0900

    commit 9063

--- baekjoon_9063.py
+++ baekjoon_9063.py
@@ -2,24 +2,13 @@
 
 N = int(input())
 
-x_min = 10001
-x_max = -10001
-y_min = 10001
-y_max = -10001
+x_list = []
+y_list = []
 
 for i in range(N):
     x, y = map(int, input().split())
 
-    if x < x_min:
-        x_min = x
+    x_list.append(x)
+    y_list.append(y)
 
-    if x > x_max:
-        x_max = x
-
-    if y < y_min:
-        y_min = y
-
-    if y > y_max:
-        y_max = y
-
-print((x_max - x_min) * (y_max - y_min))
+print((max(x_list) - min(x_list)) * (max(y_list) - min(y_list)))
