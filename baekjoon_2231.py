7b45506b54c3b7619df34ca50db7e0abd6ae51fb
Gnoyh <4ksenhs@gmail.com>
2023-09-16 21:43:01 +0900

    commit 2231

---
+++ baekjoon_2231.py
@@ -0,0 +1,19 @@
+# https://www.acmicpc.net/problem/2231
+
+import sys
+
+N = sys.stdin.readline()
+
+result = 0
+
+max_int = int(N[0]) - 1 + 9 * (len(N) - 2)
+
+for i in range(max_int, 0, -1):
+    check = int(N) - i
+
+    if i == sum(int(j) for j in str(check)):
+        result = check
+
+        break
+
+print(result)
