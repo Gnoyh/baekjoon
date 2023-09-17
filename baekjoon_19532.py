383ab20a29b44ce0d0c8cff3f666088754960863
Gnoyh <4ksenhs@gmail.com>
2023-09-17 20:37:52 +0900

    commit 19532

---
+++ baekjoon_19532.py
@@ -0,0 +1,10 @@
+# https://www.acmicpc.net/problem/19532
+
+import sys
+
+a, b, c, d, e, f = map(int, sys.stdin.readline().split())
+
+x = (c * e - b * f) // (a * e - b * d)
+y = (c * d - a * f) // (b * d - a * e)
+
+print(x, y)
