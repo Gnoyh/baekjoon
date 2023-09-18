216cd6a44ede1e5f9f36d0a3630fde0fbf6abe1c
Gnoyh <4ksenhs@gmail.com>
2023-09-18 17:21:59 +0900

    commit 1436

--- baekjoon_1436.py
+++ baekjoon_1436.py
@@ -4,15 +4,21 @@ import sys
 
 N = int(sys.stdin.readline())
 
-check = 666
+check_list = ["666"]
+result_set = set()
 
-while N != 0:
-    if "666" in str(check):
-        N -= 1
+check = N
 
-        if N == 0:
-            break
+while check > 0:
+    for i in range(10):
+        for j in check_list:
+            result_set.add(str(i) + str(j))
+            result_set.add(str(j) + str(i))
 
-    check += 1
+    check -= len(result_set)
 
-print(check)
+    check_list = list(result_set)
+
+result_set = sorted(set(map(int, result_set)))
+
+print(list(result_set)[N - 1])
