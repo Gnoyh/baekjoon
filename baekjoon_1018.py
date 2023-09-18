ca283a0d735d9e292a05171900c120ef829942dd
Gnoyh <4ksenhs@gmail.com>
2023-09-18 14:06:30 +0900

    commit 1018

--- baekjoon_1018.py
+++ baekjoon_1018.py
@@ -4,63 +4,26 @@ import sys
 
 N, M = map(int, sys.stdin.readline().split())
 
-input_list = []
-comp_list = ['B', 'W']
-
-B_list = []
-W_list = []
+input_list = [sys.stdin.readline().strip() for i in range(N)]
+check_list = [[0 for j in range(M)] for i in range(N)]
 
 for i in range(N):
-    input_list.append(sys.stdin.readline().strip())
+    for j in range(M):
+        if ((i + j) % 2 == 0 and input_list[i][j] == "W") or ((i + j) % 2 == 1 and input_list[i][j] == "B"):
+            check_list[i][j] = 1
 
-for h in range(2):
-    comp = h
+result_list = []
 
-    for i in range(N):
-        check_list = []
+for i in range(N - 7):
+    for j in range(M - 7):
+        check = 0
 
-        for j in range(7, M):
-            check = 0
+        for k in range(i, i + 8):
+            check += sum(check_list[k][j : j + 8])
 
-            for k in range(j - 7, j + 1):
-                if comp_list[comp] != input_list[i][k]:
-                    check += 1
+        result_list.append(check)
 
-                comp = abs(comp - 1)
-
-            check_list.append(check)
-
-        if h == 0:
-            B_list.append(check_list)
-        else:
-            W_list.append(check_list)
-
-BW_list = []
-WB_list = []
-
-for i in range(N):
-    if i % 2 == 0:
-        BW_list.append(B_list[i])
-        WB_list.append(W_list[i])
-    else:
-        BW_list.append(W_list[i])
-        WB_list.append(B_list[i])
-
-result = 65
-
-for i in range(len(BW_list[0])):
-    for j in range(7, N):
-        sum_BW = 0
-        sum_WB = 0
-
-        for k in range(j - 7, j + 1):
-            sum_BW += BW_list[k][i]
-            sum_WB += WB_list[k][i]
-
-        if sum_BW < result:
-            result = sum_BW
-
-        if sum_WB < result:
-            result = sum_WB
-
-print(result)
+if min(result_list) < 64 - max(result_list):
+    print(min(result_list))
+else:
+    print(64 - max(result_list))
