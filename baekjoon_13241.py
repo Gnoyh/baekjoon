2b74a6265dbba03cd9e7448789dcc70006597b77
Gnoyh <4ksenhs@gmail.com>
2023-09-27 17:15:32 +0900

    commit 13241

--- baekjoon_13241.py
+++ baekjoon_13241.py
@@ -1,8 +1,18 @@
 # https://www.acmicpc.net/problem/13241
 
 import sys
-import math
 
 A, B = map(int, sys.stdin.readline().split())
 
-print(math.lcm(A, B))
+if B > A:
+    change = A
+    A = B
+    B = change
+
+result_A = A
+result_B = B
+
+while B != 0:
+    A, B = B, A % B
+
+print(result_A * result_B // A)
