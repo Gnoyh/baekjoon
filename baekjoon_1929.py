c52a2de3d53fe922d278b8a457c009a65602d1b6
Gnoyh <4ksenhs@gmail.com>
2023-09-28 21:16:25 +0900

    commit 1929

--- baekjoon_1929.py
+++ baekjoon_1929.py
@@ -4,26 +4,16 @@ from sys import stdin
 
 M, N = map(int, stdin.readline().split())
 
-def prime(n):
-    if n == 1:
-        return False
+check_list = [1 for i in range(N + 1)]
 
-    if n == 2:
-        print(2)
+check_list[0] = 0
+check_list[1] = 0
 
-        return True
-
-    if n % 2 == 0:
-        return False
-
-    for i in range(3, int(n ** 0.5 + 1), 2):
-        if n % i == 0:
-            return False
-
-    print(n)
-
-    return True
+for i in range(2, int(N ** 0.5) + 1):
+    if check_list[i] == 1:
+        for j in range(i * i, N + 1, i):
+            check_list[j] = 0
 
 for i in range(M, N + 1):
-    if prime(i):
-        continue
+    if check_list[i] == 1:
+        print(i)
