89ac92dde9a72b427e75b9b12f0febdde394295c
Gnoyh <4ksenhs@gmail.com>
2023-09-28 18:26:42 +0900

    commit 1735

--- baekjoon_1735.py
+++ baekjoon_1735.py
@@ -1,13 +1,13 @@
 # https://www.acmicpc.net/problem/1735
 
 import sys
-import math
+from math import gcd
 
 A1, B1 = map(int, sys.stdin.readline().split())
 A2, B2 = map(int, sys.stdin.readline().split())
 
 A, B = A1 * B2 + A2 * B1, B1 * B2
 
-check = math.gcd(A, B)
+check = gcd(A, B)
 
 print(A // check, B // check)
