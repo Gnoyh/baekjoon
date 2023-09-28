8f9e0f8254c4d07e987a128d4b6521a1b92908e0
Gnoyh <4ksenhs@gmail.com>
2023-09-28 20:44:43 +0900

    commit 4134

--- baekjoon_4134.py
+++ baekjoon_4134.py
@@ -4,30 +4,28 @@ from sys import stdin
 
 count = int(stdin.readline())
 
+def prime(n):
+    if n <= 2:
+        print(2)
+
+        return True
+
+    if n % 2 == 0:
+        return False
+
+    for i in range(3, int(n ** 0.5 + 1), 2):
+        if n % i == 0:
+            return False
+
+    print(n)
+
+    return True
+
 for i in range(count):
     n = int(stdin.readline())
 
     while True:
-        check = 0
-
-        if n <= 2:
-            print(2)
-
+        if prime(n):
             break
 
-        if n % 2 == 0:
-            n += 1
-
-            continue
-
-        for j in range(3, int(n ** 0.5 + 1), 2):
-            if n % j == 0:
-                n += 1
-                check = 1
-
-                break
-
-        if check == 0:
-            print(n)
-
-            break
+        n += 1
