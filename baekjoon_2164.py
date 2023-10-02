627b48e7836dce6911f265596e88c6dc563baa14
Gnoyh <4ksenhs@gmail.com>
2023-10-02 23:48:20 +0900

    commit 2164

--- baekjoon_2164.py
+++ baekjoon_2164.py
@@ -4,20 +4,18 @@ from sys import stdin
 
 N = int(stdin.readline())
 
-input_list = list(i for i in range(1, N + 1))
-
-def card(input_list, n):
-    check_list = list()
-
-    if n % 2 == 1:
-        check_list.append(input_list[-1])
-
-    for i in range(1, n, 2):
-        check_list.append(input_list[i])
-
-    if len(check_list) == 1:
-        print(check_list[0])
+def card():
+    if N < 3:
+        print(N)
     else:
-        card(check_list, len(check_list))
+        check = 4
 
-card(input_list, N)
+        while True:
+            if N <= check:
+                print((N - check // 2) * 2)
+
+                break
+
+            check *= 2
+
+card()
