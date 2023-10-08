30910dac3fc3471fef5453483add7ea3a650fab0
Gnoyh <4ksenhs@gmail.com>
2023-10-08 21:32:14 +0900

    commit 20920

--- baekjoon_20920.py
+++ baekjoon_20920.py
@@ -1,27 +1,25 @@
 # https://www.acmicpc.net/problem/20920
 
 from sys import stdin
+from collections import Counter
 
 N, M = map(int, stdin.readline().split())
 
 def eng_memo():
-    input_dict = dict()
-    result_list = list()
+    input_list = list()
 
     for _ in range(N):
         input_str = stdin.readline().rstrip()
 
         if len(input_str) >= M:
-            if input_str in input_dict:
-                input_dict[input_str] += 1
-            else:
-                input_dict[input_str] = 1
+            input_list.append(input_str)
 
-    input_dict = sorted(input_dict.items(), key=lambda x: (-x[1], -len(x[0]), x[0]))
+    input_count = Counter(input_list)
 
-    for i in input_dict:
-        result_list.append(i[0])
+    input_list = sorted(input_count)
+    input_list = sorted(input_list, key=len, reverse=True)
+    input_list = sorted(input_list, key=input_count.get, reverse=True)
 
-    print("\n".join(result_list))
+    print("\n".join(input_list))
 
 eng_memo()
