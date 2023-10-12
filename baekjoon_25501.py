a7fb59ce0550a93f09a3658476f47f461644d376
Gnoyh <4ksenhs@gmail.com>
2023-10-12 10:04:52 +0900

    commit 25501

--- baekjoon_25501.py
+++ baekjoon_25501.py
@@ -4,24 +4,25 @@ from sys import stdin
 
 T = int(stdin.readline())
 
-def recursion(s, l, r, check_recur):
-    check_recur += 1
-
-    if l >= r:
-        return [1, check_recur]
-    elif s[l] != s[r]:
-        return [0, check_recur]
-    else:
-        return recursion(s, l + 1, r - 1, check_recur)
-
-def isPalindrome(s):
-    return recursion(s, 0, len(s) - 1, 0)
-
-def return_count():
+def recursion():
     for _ in range(T):
         input_str = stdin.readline().rstrip()
 
-        print(isPalindrome(input_str)[0], end=" ")
-        print(isPalindrome(input_str)[1])
+        result = 0
+        check = 0
 
-return_count()
+        for i in range(len(input_str)):
+            check += 1
+
+            if i >= len(input_str) - i - 1:
+                result = 1
+
+                break
+            elif input_str[i] != input_str[len(input_str) - i - 1]:
+                result = 0
+
+                break
+
+        print(result, check)
+
+recursion()
