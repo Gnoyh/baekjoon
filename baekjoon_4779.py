9280ba212f5a1645157e65d27f0614f708f388f3
Gnoyh <4ksenhs@gmail.com>
2023-10-16 18:04:58 +0900

    commit 4779

---
+++ baekjoon_4779.py
@@ -0,0 +1,19 @@
+# https://www.acmicpc.net/problem/4779
+
+from sys import stdin
+
+def cantorian(start, end, input_str):
+    if N == 1:
+        return
+
+    for i in range((end - start + 1) // 3, (end - start + 1) // 3 * 2):
+
+while True:
+    try:
+        N = int(stdin.readline())
+
+        input_str = ["-"] * (3 ** N)
+
+        cantorian()
+    except:
+        break
