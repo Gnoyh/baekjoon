176679cf2717fc85aa0e54e6df4cdda652a84561
Gnoyh <4ksenhs@gmail.com>
2023-10-19 11:22:10 +0900

    commit 2447

--- baekjoon_2447.py
+++ baekjoon_2447.py
@@ -3,28 +3,30 @@
 from sys import stdin
 
 N = int(stdin.readline())
+check = 0
+
+while N != 1:
+    N = N // 3
+
+    check += 1
 
 def star():
     result_list = [["*"]]
 
-    check = 1
-
-    while (3 ** check) <= N:
+    for i in range(1, check + 1):
         check_list = []
 
-        for j in result_list[check - 1]:
+        for j in result_list[i - 1]:
             check_list.append(j * 3)
 
-        for j in result_list[check - 1]:
-            check_list.append(j + " " * (3 ** (check - 1)) + j)
+        for j in result_list[i - 1]:
+            check_list.append(j + " " * (3 ** (i - 1)) + j)
 
-        for j in result_list[check - 1]:
+        for j in result_list[i - 1]:
             check_list.append(j * 3)
 
         result_list.append(check_list)
 
-        check += 1
-
-    print("\n".join(result_list[check - 1]))
+    print("\n".join(result_list[check]))
 
 star()
