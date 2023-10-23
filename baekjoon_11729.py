05bdf8c8fb094343d247ac4932f18a3f3333c3dd
Gnoyh <4ksenhs@gmail.com>
2023-10-23 14:58:55 +0900

    commit 11729

--- baekjoon_11729.py
+++ baekjoon_11729.py
@@ -2,3 +2,23 @@
 
 from sys import stdin
 
+K = int(stdin.readline())
+
+result_list = []
+
+def hanoi(k, bar1, bar2, bar3):
+    if k == 1:
+        result_list.append(str(bar1) + " " + str(bar2))
+
+        return
+
+    hanoi(k - 1, bar1, bar3, bar2)
+
+    result_list.append(str(bar1) + " " + str(bar2))
+
+    hanoi(k - 1, bar3, bar2, bar1)
+
+hanoi(K, 1, 3, 2)
+
+print(len(result_list))
+print("\n".join(result_list))
