f6035006feb0d310bdd70cac19e0cdcd4c6546ec
Gnoyh <4ksenhs@gmail.com>
2023-11-01 22:02:33 +0900

    commit 14889

--- baekjoon_14889.py
+++ baekjoon_14889.py
@@ -6,14 +6,23 @@ N = int(stdin.readline())
 
 input_list = [list(map(int, stdin.readline().split())) for _ in range(N)]
 
+check = 0
 result = 0
 result_min = -1
 
-def startlink(check_list, check_set, check):
+def startlink(check_list, check_set, count):
+    global check
     global result
     global result_min
 
-    if len(check_set) == N // 2:
+    if count == N // 2:
+        check = 0
+
+        for i in check_set:
+            for j in check_set:
+                if i != j:
+                    check += input_list[i][j]
+
         if result == 0:
             result = check
 
@@ -31,15 +40,14 @@ def startlink(check_list, check_set, check):
 
             return
 
-    for i in check_list:
-        if i not in check_set:
-            for j in check_set:
-                check += input_list[i][j] + input_list[j][i]
+    for i in range(count, len(check_list)):
+        if check_list[i] not in check_set:
+            check_set.add(check_list[i])
 
-            check_set.add(i)
+            startlink(check_list, check_set, count + 1)
 
-            startlink(check_list, check_set, check)
-
-            check_set.discard(i)
+            check_set.discard(check_list[i])
 
 startlink([i for i in range(N)], set(), 0)
+
+print(result_min)
