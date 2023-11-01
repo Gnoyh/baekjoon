faa3b470c5468fbe32ad56ae9a6d2f0b6e2fa873
Gnoyh <4ksenhs@gmail.com>
2023-11-01 22:28:36 +0900

    commit 14889

--- baekjoon_14889.py
+++ baekjoon_14889.py
@@ -8,42 +8,41 @@ input_list = [list(map(int, stdin.readline().split())) for _ in range(N)]
 check_list = [i for i in range(N)]
 check_set = set()
 
-result = -1
+result_list = []
+
+check_start = 0
 
 def startlink(count):
-    global result
+    global check_start
 
     if count == N // 2:
-        check_start = 0
         check_link = 0
 
-        check_startlist = list(check_set)
         check_linklist = [i for i in check_list if i not in check_set]
-
-        for i in range(N // 2):
-            for j in range(i + 1, N // 2):
-                check_start += input_list[check_startlist[i]][check_startlist[j]] + input_list[check_startlist[j]][check_startlist[i]]
 
         for i in range(N // 2):
             for j in range(i + 1, N // 2):
                 check_link += input_list[check_linklist[i]][check_linklist[j]] + input_list[check_linklist[j]][check_linklist[i]]
 
-        if result == -1:
-            result = abs(check_start - check_link)
-        else:
-            if result > abs(check_start - check_link):
-                result = abs(check_start - check_link)
+        result_list.append(abs(check_start - check_link))
 
         return
 
     for i in range(count, len(check_list)):
         if check_list[i] not in check_set:
+            check = 0
+
+            for j in check_set:
+                check += input_list[i][j] + input_list[j][i]
+
+            check_start += check
             check_set.add(check_list[i])
 
             startlink(count + 1)
 
+            check_start -= check
             check_set.discard(check_list[i])
 
 startlink(0)
 
-print(result)
+print(min(result_list))
