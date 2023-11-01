a8531bf69d3a0c2911df2a92587e82ad34532b63
Gnoyh <4ksenhs@gmail.com>
2023-11-01 22:13:48 +0900

    commit 14889

--- baekjoon_14889.py
+++ baekjoon_14889.py
@@ -5,49 +5,45 @@ from sys import stdin
 N = int(stdin.readline())
 
 input_list = [list(map(int, stdin.readline().split())) for _ in range(N)]
+check_list = [i for i in range(N)]
+check_set = set()
 
-check = 0
-result = 0
-result_min = -1
+result = -1
 
-def startlink(check_list, check_set, count):
-    global check
+def startlink(count):
     global result
-    global result_min
 
     if count == N // 2:
-        check = 0
+        check_start = 0
+        check_link = 0
 
-        for i in check_set:
-            for j in check_set:
-                if i != j:
-                    check += input_list[i][j]
+        check_startlist = list(check_set)
+        check_linklist = [i for i in check_list if i not in check_set]
 
-        if result == 0:
-            result = check
+        for i in range(N // 2):
+            for j in range(i + 1, N // 2):
+                check_start += input_list[check_startlist[i]][check_startlist[j]] + input_list[check_startlist[j]][check_startlist[i]]
 
-            startlink([i for i in check_list if i not in check_set], set(), 0)
+        for i in range(N // 2):
+            for j in range(i + 1, N // 2):
+                check_link += input_list[check_linklist[i]][check_linklist[j]] + input_list[check_linklist[j]][check_linklist[i]]
 
-            return
+        if result == -1:
+            result = abs(check_start - check_link)
         else:
-            if result_min == -1:
-                result_min = abs(result - check)
-            else:
-                if result_min > abs(result - check):
-                    result_min = abs(result - check)
+            if result > abs(check_start - check_link):
+                result = abs(check_start - check_link)
 
-            result = 0
-
-            return
+        return
 
     for i in range(count, len(check_list)):
         if check_list[i] not in check_set:
             check_set.add(check_list[i])
 
-            startlink(check_list, check_set, count + 1)
+            startlink(count + 1)
 
             check_set.discard(check_list[i])
 
-startlink([i for i in range(N)], set(), 0)
+startlink(0)
 
-print(result_min)
+print(result)
