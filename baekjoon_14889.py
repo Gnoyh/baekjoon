8e17ca3c38fb0f40acc49862db6730181061b701
Gnoyh <4ksenhs@gmail.com>
2023-11-05 22:28:30 +0900

    commit 14889

--- baekjoon_14889.py
+++ baekjoon_14889.py
@@ -11,32 +11,29 @@ visited_list = [0 for i in range(N)]
 
 result = sys.maxsize
 
-def calculate():
+def startlink(count):
     global result
 
-    start_list = []
-    link_list = []
-
-    check_start = 0
-    check_link = 0
-
-    for i in range(N):
-        if visited_list[i] == 1:
-            start_list.append(i)
-        else:
-            link_list.append(i)
-
-    for i in range(N // 2):
-        for j in range(i + 1, N // 2):
-            check_start += input_list[start_list[i]][start_list[j]] + input_list[start_list[j]][start_list[i]]
-            check_link += input_list[link_list[i]][link_list[j]] + input_list[link_list[j]][link_list[i]]
-
-    if result > abs(check_start - check_link):
-        result = abs(check_start - check_link)
-
-def startlink(count):
     if count == N // 2:
-        calculate()
+        start_list = []
+        link_list = []
+
+        check_start = 0
+        check_link = 0
+
+        for i in range(N):
+            if visited_list[i] == 1:
+                start_list.append(i)
+            else:
+                link_list.append(i)
+
+        for i in range(N // 2):
+            for j in range(i + 1, N // 2):
+                check_start += input_list[start_list[i]][start_list[j]] + input_list[start_list[j]][start_list[i]]
+                check_link += input_list[link_list[i]][link_list[j]] + input_list[link_list[j]][link_list[i]]
+
+        if result > abs(check_start - check_link):
+            result = abs(check_start - check_link)
 
         return
 
