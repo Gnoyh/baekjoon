15c95f29da17a2093065b2bd3833d7427a2df359
Gnoyh <4ksenhs@gmail.com>
2023-11-06 12:34:08 +0900

    commit 14889

--- baekjoon_14889.py
+++ baekjoon_14889.py
@@ -1,13 +1,11 @@
 # https://www.acmicpc.net/problem/14889
 
 import sys
-from sys import stdin
 
-N = int(stdin.readline())
+N = int(sys.stdin.readline())
 
-input_list = [list(map(int, stdin.readline().split())) for _ in range(N)]
-check_list = [i for i in range(N)]
-visited_list = [0 for i in range(N)]
+input_list = [list(map(int, sys.stdin.readline().split())) for _ in range(N)]
+visited_list = [False] * N
 
 result = sys.maxsize
 
@@ -15,22 +13,16 @@ def startlink(count):
     global result
 
     if count == N // 2:
-        start_list = []
-        link_list = []
-
         check_start = 0
         check_link = 0
 
         for i in range(N):
-            if visited_list[i] == 1:
-                start_list.append(i)
-            else:
-                link_list.append(i)
+            for j in range(i + 1, N):
+                if visited_list[i] and visited_list[j]:
+                    check_start += input_list[i][j] + input_list[j][i]
 
-        for i in range(N // 2):
-            for j in range(i + 1, N // 2):
-                check_start += input_list[start_list[i]][start_list[j]] + input_list[start_list[j]][start_list[i]]
-                check_link += input_list[link_list[i]][link_list[j]] + input_list[link_list[j]][link_list[i]]
+                if not visited_list[i] and not visited_list[j]:
+                    check_link += input_list[i][j] + input_list[j][i]
 
         if result > abs(check_start - check_link):
             result = abs(check_start - check_link)
@@ -38,12 +30,12 @@ def startlink(count):
         return
 
     for i in range(count, N):
-        if visited_list[i] == 0:
-            visited_list[i] = 1
+        if not visited_list[i]:
+            visited_list[i] = True
 
             startlink(count + 1)
 
-            visited_list[i] = 0
+            visited_list[i] = False
 
 startlink(0)
 
