a5316521ed8dc3484be6bfbed4c962e4aeaee44b
Gnoyh <4ksenhs@gmail.com>
2023-11-06 11:44:25 +0900

    commit 9663

--- baekjoon_9663.py
+++ baekjoon_9663.py
@@ -6,11 +6,9 @@ N = int(stdin.readline())
 
 result = 0
 
-def nqueen(check):
+def nqueen(check, check_second, check_third):
     global result
     global check_first
-    global check_second
-    global check_third
 
     if check == N:
         result += 1
@@ -24,29 +22,18 @@ def nqueen(check):
             if check_second[i] or check_third[i]:
                 continue
             else:
-                check_secondid = check_second[0]
-                check_thirdid = check_third[-1]
-
                 check_first[i] = True
                 check_second[i] = True
                 check_third[i] = True
 
-                check_second = check_second[1: ] + [False]
-                check_third = [False] + check_third[: -1]
-
-                nqueen(check + 1)
-
-                check_second = [check_secondid] + check_second[: -1]
-                check_third = check_third[1: ] + [check_thirdid]
+                nqueen(check + 1, check_second[1: ] + [False], [False] + check_third[: -1])
 
                 check_first[i] = False
                 check_second[i] = False
                 check_third[i] = False
 
 check_first = [False] * N
-check_second = [False] * N
-check_third = [False] * N
 
-nqueen(0)
+nqueen(0, [False] * N, [False] * N)
 
 print(result)
