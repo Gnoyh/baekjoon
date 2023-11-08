616e988a4038625a62d9bb2d0ae955ffd23554d1
Gnoyh <4ksenhs@gmail.com>
2023-11-08 18:17:19 +0900

    commit 9663

--- baekjoon_9663.py
+++ baekjoon_9663.py
@@ -6,6 +6,17 @@ N = int(stdin.readline())
 
 result = 0
 
+def truefalse(i, check):
+    check_first[i] = True
+    check_second[i - check + N - 1] = True
+    check_third[i + check] = True
+
+    nqueen(check + 1)
+
+    check_first[i] = False
+    check_second[i - check + N - 1] = False
+    check_third[i + check] = False
+
 def nqueen(check):
     global result
 
@@ -21,20 +32,23 @@ def nqueen(check):
             if check_second[i - check + N - 1] or check_third[i + check]:
                 continue
             else:
-                check_first[i] = True
-                check_second[i - check + N - 1] = True
-                check_third[i + check] = True
-
-                nqueen(check + 1)
-
-                check_first[i] = False
-                check_second[i - check + N - 1] = False
-                check_third[i + check] = False
+                truefalse(i, check)
 
 check_first = [False] * N
 check_second = [False] * (N * 2 - 1)
 check_third = [False] * (N * 2 - 1)
 
-nqueen(0)
+if N % 2 == 0:
+    for i in range(N // 2):
+        truefalse(i, 0)
 
-print(result)
+    print(result * 2)
+else:
+    for i in range(N // 2):
+        truefalse(i, 0)
+
+    result *= 2
+
+    truefalse(N // 2, 0)
+
+    print(result)
