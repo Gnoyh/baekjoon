de8bf62cb28a88865648cab9f03f32598a2d8524
Gnoyh <4ksenhs@gmail.com>
2023-11-11 21:46:00 +0900

    commit 2580

--- baekjoon_2580.py
+++ baekjoon_2580.py
@@ -10,56 +10,44 @@ for i in range(9):
         if input_list[i][j] == 0:
             check_list.append((i, j))
 
-def txt(x):
-    if x in {0, 1, 2}:
-        return [0, 1, 2]
-    elif x in {3, 4, 5}:
-        return [3, 4, 5]
-    else:
-        return[6, 7, 8]
+def row(i, x):
+    for j in input_list[x]:
+        if j == i:
+            return False
+
+    return True
+
+def column(i, y):
+    for j in range(9):
+        if input_list[j][y] == i:
+            return False
+
+    return True
+
+def square(i, x, y):
+    for j in range(3 * (x // 3), 3 * (x // 3) + 3):
+        for k in range(3 * (y // 3), 3 * (y // 3) + 3):
+            if input_list[j][k] == i:
+                return False
+
+    return True
 
 def sudoku(count):
     if count == len(check_list):
         for i in range(9):
             print(*input_list[i])
 
-        exit(0)
+        exit()
+
+    x = check_list[count][0]
+    y = check_list[count][1]
 
     for i in range(1, 10):
-        check = 1
+        if row(i, x) and column(i, y) and square(i, x, y):
+            input_list[check_list[count][0]][check_list[count][1]] = i
 
-        for j in input_list[check_list[count][0]]:
-            if j == i:
-                check = 0
+            sudoku(count + 1)
 
-                break
-
-        if check == 0:
-            continue
-
-        for j in range(9):
-            if input_list[j][check_list[count][1]] == i:
-                check = 0
-
-                break
-
-        if check == 0:
-            continue
-
-        for j in txt(check_list[count][0]):
-            for k in txt(check_list[count][1]):
-                if input_list[j][k] == i:
-                    check = 0
-
-                    break
-
-        if check == 0:
-            continue
-
-        input_list[check_list[count][0]][check_list[count][1]] = i
-
-        sudoku(count + 1)
-
-        input_list[check_list[count][0]][check_list[count][1]] = 0
+            input_list[check_list[count][0]][check_list[count][1]] = 0
 
 sudoku(0)
