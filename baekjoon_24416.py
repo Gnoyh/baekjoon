205d5a0789163d29b2315ae6f2bd4cbd1174d029
Gnoyh <4ksenhs@gmail.com>
2025-01-30 18:53:57 +0900

    commit 24416

---
+++ baekjoon_24416.py
@@ -0,0 +1,10 @@
+# https://www.acmicpc.net/problem/24416
+
+n = int(input())
+
+f = [0, 1, 1] + ([0] * (n - 2))
+
+for i in range(3, n + 1):
+    f[i] = f[i - 1] + f[i - 2]
+    
+print(f[n], n - 2)
