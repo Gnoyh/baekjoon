857dafef5a29e789169a5a65ec3ec41983ea4981
Gnoyh <4ksenhs@gmail.com>
2025-02-07 22:21:24 +0900

    commit 2565

---
+++ baekjoon_2565.py
@@ -0,0 +1,27 @@
+# https://www.acmicpc.net/problem/2565
+
+n = int(input())
+
+num_list = []
+num_dict = dict()
+
+for i in range(n):
+    a, b = map(int, input().split())
+    
+    num_list.append(a)
+    num_dict[a] = b
+    
+num_list.sort()
+
+check_list = [1]
+
+for i in range(1, n):
+    num = 1
+    
+    for j in range(i):
+        if num_dict[num_list[i]] > num_dict[num_list[j]]:
+            num = max(num, check_list[j] + 1)
+            
+    check_list.append(num)
+        
+print(n - max(check_list))
