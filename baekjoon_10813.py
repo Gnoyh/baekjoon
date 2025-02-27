a74a2aba2145923004264f714f3020d4f1d30cd1
Gnoyh <127109635+Gnoyh@users.noreply.github.com>
2025-02-27 21:07:39 +0900

    commit 10813

--- baekjoon_10813.py
+++ baekjoon_10813.py
@@ -1,15 +1,16 @@
 # https://www.acmicpc.net/problem/10813
 
+import sys
 import copy
 
-N, M = map(int, input().split())
+N, M = map(int, sys.stdin.readline().strip().split())
 
 result_list = [i + 1 for i in range(N)]
 
 for x in range(M):
     check_list = copy.deepcopy(result_list)
 
-    i, j = map(int, input().split())
+    i, j = map(int, sys.stdin.readline().strip().split())
 
     check_list[i - 1] = result_list[j - 1]
     check_list[j - 1] = result_list[i - 1]
