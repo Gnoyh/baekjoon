9d36991a1ca3d480ce8ce6117565fffaafc71d45
Gnoyh <127109635+Gnoyh@users.noreply.github.com>
2025-03-19 20:00:27 +0900

    commit 5597

--- baekjoon_5597.py
+++ baekjoon_5597.py
@@ -1,9 +1,11 @@
 # https://www.acmicpc.net/problem/5597
 
+import sys
+
 check_list = [0 for i in range(30)]
 
 for i in range(28):
-    n = int(input())
+    n = int(sys.stdin.readline().strip())
 
     check_list[n - 1] = 1
 
